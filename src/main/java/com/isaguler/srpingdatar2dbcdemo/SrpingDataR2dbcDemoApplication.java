46691da696ae5ddd319838e074b71fa6dd2d6179
Gülers <jesus3419@gmail.com>
2022-12-23 20:11:25 +0300

    Initial Commit

---
+++ src/main/java/com/isaguler/srpingdatar2dbcdemo/SrpingDataR2dbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.isaguler.srpingdatar2dbcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingDataR2dbcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingDataR2dbcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaguler/srpingdatar2dbcdemo/SrpingDataR2dbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaguler.srpingdatar2dbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingDataR2dbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
